434f30e2703e432b061650f0d1716ccf85cc513c
thegostisdead <thegostisdead@gmail.com>
2023-09-06 19:28:29 +0200

    add pages

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,13 +10,14 @@ export default async function DashboardLayout({children}: {
     const session = await getServerSession();
 
     if (!session) {
-        redirect('/api/auth/signin');
+        // redirect('/api/auth/signin');
         // return <p>You must be signed in...</p>
     }
 
     return (
-        <div>
-            <div className="border-b">
+        <div className="flex justify-center h-screen w-full flex-wrap">
+
+            <div className="flex border-b w-full">
                 <div className="flex h-16 items-center px-4">
                     <Nav  className="mx-6" />
                     <div className="ml-auto flex items-center space-x-4">
@@ -24,7 +25,9 @@ export default async function DashboardLayout({children}: {
                     </div>
                 </div>
             </div>
-            {children}
+            <div className={"flex"}>
+                {children}
+            </div>
         </div>
     )
 }
